a453826da1deb1f8bab52c48c293dfa55fad0e92
Abhinav Chaudhary <abhinav.chaudhary@tothenew.com>
2025-08-12 10:17:25 +0530

    Initial Commit

---
+++ src/test/java/com/abhinav/abhinavproject/AbhinavProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinav.abhinavproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbhinavProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
