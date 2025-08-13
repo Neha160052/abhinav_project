e9b56b52c32416d1ee18fdc2a3a5632642f18c98
Abhinav Chaudhary <abhinav.chaudhary@tothenew.com>
2025-08-13 11:31:43 +0530

    refactor: Enabled JPA Auditing for application and created a dummy AuditorAwareImpl for auditing.

--- src/main/java/com/abhinav/abhinavProject/AbhinavProjectApplication.java
+++ src/main/java/com/abhinav/abhinavProject/AbhinavProjectApplication.java
@@ -2,8 +2,10 @@ package com.abhinav.abhinavProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class AbhinavProjectApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/abhinav/abhinavProject/AbhinavProjectApplicationTests.java
+++ src/test/java/com/abhinav/abhinavProject/AbhinavProjectApplicationTests.java
@@ -1,15 +1,11 @@
 package com.abhinav.abhinavProject;
 
-import com.abhinav.abhinavProject.repository.CategoryMetadataFieldValuesRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AbhinavProjectApplicationTests {
-
-    @Autowired
-    CategoryMetadataFieldValuesRepository repo;
 
     @Test
     void contextLoads() {
