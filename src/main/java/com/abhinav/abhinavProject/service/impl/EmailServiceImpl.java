1c0b90d1c8d644e76f94fd14a31270193420a4c9
Abhinav Chaudhary <abhinav.chaudhary@tothenew.com>
2025-08-25 22:59:32 +0530

    refactor: Improved EmailServiceImpl to fix code repetition.

--- src/main/java/com/abhinav/abhinavProject/service/impl/EmailServiceImpl.java
+++ src/main/java/com/abhinav/abhinavProject/service/impl/EmailServiceImpl.java
@@ -27,68 +27,41 @@ public class EmailServiceImpl {
             mail.setText(body);
 
             javaMailSender.send(mail);
+            log.info("Sent email to: " + to);
         } catch (Exception e) {
             log.error("Error sending mail");
         }
     }
 
     public void sendActivationEmail(String firstName, String toMail, String token) {
-        try {
-            SimpleMailMessage mail = new SimpleMailMessage();
-            mail.setTo(toMail);
-            mail.setSubject("Activate your account");
-            mail.setText("Hi "+firstName+",\nPlease activate your account by clicking the link below:\n\nhttp://localhost:8080/api/customer/activate?token="+token);
-
-            javaMailSender.send(mail);
-
-            log.info("Sent email to email: "+toMail);
-        } catch (Exception e) {
-            log.error("Error sending mail");
-        }
+        sendMail(
+                toMail,
+                "Activate your account",
+                "Hi " + firstName + ",\nPlease activate your account by clicking the link below:\n\nhttp://localhost:8080/api/customer/activate?token=" + token
+        );
     }
 
     public void sendPasswordResetEmail(String firstName, String toMail, String token) {
-        try {
-            SimpleMailMessage mail = new SimpleMailMessage();
-            mail.setTo(toMail);
-            mail.setSubject("Password Reset Request");
-            mail.setText("Hi "+firstName+",\nPlease reset your password by clicking the link below:\n\nhttp://localhost:8080/api/auth/forgot-password/reset?token="+token);
-
-            javaMailSender.send(mail);
-
-            log.info("Sent email to email: "+toMail);
-        } catch (Exception e) {
-            log.error("Error sending mail");
-        }
+        sendMail(
+                toMail,
+                "Password Reset Request",
+                "Hi "+firstName+",\nPlease reset your password by clicking the link below:\n\nhttp://localhost:8080/api/auth/forgot-password/reset?token=" + token
+        );
     }
 
     public void sendAdminActivationMail(User user) {
-        try {
-            SimpleMailMessage mail = new SimpleMailMessage();
-            mail.setTo(user.getEmail());
-            mail.setSubject("Account activation successful");
-            mail.setText("Hi "+user.getFirstName()+",\nYour account has been successfully activated by the Admin.");
-
-            javaMailSender.send(mail);
-
-            log.info("Sent email to email: "+user.getEmail());
-        } catch (Exception e) {
-            log.error("Error sending mail");
-        }
+        sendMail(
+                user.getEmail(),
+                "Account activation successful",
+                "Hi "+user.getFirstName()+",\nYour account has been successfully activated by the Admin."
+        );
     }
 
     public void sendAdminDeactivationMail(User user) {
-        try {
-            SimpleMailMessage mail = new SimpleMailMessage();
-            mail.setTo(user.getEmail());
-            mail.setSubject("Account deactivated");
-            mail.setText("Hi "+user.getFirstName()+",\nYour account has been deactivated by the Admin.");
-
-            javaMailSender.send(mail);
-
-            log.info("Sent email to email: "+user.getEmail());
-        } catch (Exception e) {
-            log.error("Error sending mail");
-        }
+        sendMail(
+                user.getEmail(),
+                "Account deactivated",
+                "Hi " + user.getFirstName() + ",\nYour account has been deactivated by the Admin."
+        );
     }
 }
