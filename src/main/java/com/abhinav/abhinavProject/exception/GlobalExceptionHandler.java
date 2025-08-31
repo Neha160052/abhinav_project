2622ae0e31aebe0a48483d4b1c53d2f08a5ab7b5
Abhinav Chaudhary <abhinav.chaudhary@tothenew.com>
2025-08-31 13:22:46 +0530

    feat: Implemented i18n for GlobalExceptionHandler

--- src/main/java/com/abhinav/abhinavProject/exception/GlobalExceptionHandler.java
+++ src/main/java/com/abhinav/abhinavProject/exception/GlobalExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.abhinav.abhinavProject.exception;
 
+import com.abhinav.abhinavProject.utils.MessageUtil;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AccountStatusException;
@@ -14,7 +18,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+
+    MessageUtil messageUtil;
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiResponse> validationExceptionHandler(MethodArgumentNotValidException e) {
@@ -24,28 +32,28 @@ public class GlobalExceptionHandler {
                 .forEach(fieldError -> errors.put(fieldError.getField(), fieldError.getDefaultMessage()));
 
         return ResponseEntity.badRequest().body(
-                new ApiResponse(HttpStatus.BAD_REQUEST.value(), "Validation Failed", errors)
+                new ApiResponse(HttpStatus.BAD_REQUEST.value(), messageUtil.getMessage("validation.failed"), errors)
         );
     }
 
     @ExceptionHandler(AuthenticationCredentialsNotFoundException.class)
     public ResponseEntity<ApiResponse> handleMissingCredentials(AuthenticationCredentialsNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
-                new ApiResponse(HttpStatus.UNAUTHORIZED.value(), ex.getMessage(), "Incomplete authentication")
+                new ApiResponse(HttpStatus.UNAUTHORIZED.value(), ex.getMessage(), messageUtil.getMessage("auth.credentials.missing"))
         );
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ApiResponse> handleUserNotFound(UserNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-                new ApiResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), "User does not exist.")
+                new ApiResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), messageUtil.getMessage("user.notfound"))
         );
     }
 
     @ExceptionHandler(AddressNotFoundException.class)
     public ResponseEntity<ApiResponse> handleAddressNotFound(AddressNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-                new ApiResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), "The requested address does not exist")
+                new ApiResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), messageUtil.getMessage("address.notfound"))
         );
     }
 
@@ -53,34 +61,34 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(AccountInactiveException.class)
     public ResponseEntity<ApiResponse> handleAccountInactive(AccountInactiveException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                new ApiResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), "Account is not active")
+                new ApiResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), messageUtil.getMessage("account.inactive"))
         );
     }
 
     @ExceptionHandler(PasswordMismatchException.class)
     public ResponseEntity<ApiResponse> handlePasswordMismatch(PasswordMismatchException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                new ApiResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), "Passwords do not match")
+                new ApiResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), messageUtil.getMessage("password.mismatch"))
         );
     }
 
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<ApiResponse> handleBadCredentials(BadCredentialsException ex) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
-                new ApiResponse(HttpStatus.UNAUTHORIZED.value(), ex.getMessage(), "Username or password is incorrect")
+                new ApiResponse(HttpStatus.UNAUTHORIZED.value(), ex.getMessage(), messageUtil.getMessage("bad.credentials"))
         );
     }
 
     @ExceptionHandler(AccountStatusException.class)
     public ResponseEntity<ApiResponse> handleAccountStatus(AccountStatusException ex) {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(
-                new ApiResponse(HttpStatus.FORBIDDEN.value(), ex.getMessage(), "User Account is locked or inactive")
+                new ApiResponse(HttpStatus.FORBIDDEN.value(), ex.getMessage(), messageUtil.getMessage("account.status"))
         );
     }
 
     @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
     public ResponseEntity<ApiResponse> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex) {
-        String message = "Media type not supported. Supported types are: " + ex.getSupportedMediaTypes();
+        String message = messageUtil.getMessage("media.unsupported") + ex.getSupportedMediaTypes();
         return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(
                 new ApiResponse(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value(), ex.getMessage(), message)
         );
@@ -90,7 +98,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiResponse> respond(RuntimeException e) {
         return ResponseEntity
                 .internalServerError()
-                .body(new ApiResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage(), "Internal Server Error")
+                .body(new ApiResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage(), messageUtil.getMessage("server.error"))
                 );
     }
 }
